29e22c47d3bc75904e1e96768d22af4e177d99d7
adamskipadamski <adam.t.wright@outlook.com>
2022-11-02 22:30:29 +0000

    add Conductor component

---
+++ Assets/Resources/Scripts/Conductor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    public int BPM;
+    public float[] BeatLocations = { 1, 2, 3 };
+
+    private const int BeatLength = 100;
+    private List<GameObject> Beats = new();
+
+    void Start()
+    {
+        for (int i = 0; i < BeatLocations.Length; i++)
+        {
+            float beatLocation = BeatLocations[i] * BeatLength;
+
+            GameObject beat = new GameObject();
+            beat.transform.position = new Vector3(beatLocation, 0, 0);
+
+            Beats.Add(beat);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < BeatLocations.Length; i++)
+        {
+            GameObject beat = Beats[i];
+
+            Vector3 displacement = BeatLength * Vector3.left * Time.deltaTime;
+            beat.transform.position = beat.transform.position + displacement;
+        }
+    }
+}
