540719751fda10f7bfdaca0bbca06fc33ac40f91
Megan <megan@theparkes.me.uk>
2022-11-02 14:18:17 +0000

    forest

---
+++ Assets/Resources/Scripts/Enviroment/BackgroundGenerator.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGenerator : MonoBehaviour
+{
+    public List<Sprite> Sprites;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
