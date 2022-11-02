390ee414dbbb7f31a3b4684823077cf9ab67d2f9
Megan <megan@theparkes.me.uk>
2022-11-02 16:13:37 +0000

    wip background generator

--- Assets/Resources/Scripts/Enviroment/BackgroundGenerator.cs
+++ Assets/Resources/Scripts/Enviroment/BackgroundGenerator.cs
@@ -4,10 +4,48 @@ using UnityEngine;
 public class BackgroundGenerator : MonoBehaviour
 {
     public List<Sprite> Sprites;
-    
+    public float Speed = 1;
+
+    public int CanvasWidth = 480;
+    private int tileWidth = 10;
+    private int tilesPerPage = 0;
+
+    private List<Tile> tiles = new List<Tile>();
+
+    private int getEndPosition() => CanvasWidth;
+
+
     void Start()
     {
-        
+        tileWidth = Mathf.RoundToInt(Sprites[0].bounds.size.x);
+        tilesPerPage = Mathf.RoundToInt(getEndPosition() / tileWidth) + 2 ; // extra tiles to handle edge of page
+        for (int i = 0; i < tilesPerPage; i++)
+        {
+            Tile prevTile = i == 0 ? null : tiles[i - 1];
+            Tile tile = AddTile(tileWidth * i, prevTile);
+            tiles.Add(tile);
+        }
+
+        tiles[0].PrevTile = tiles[tiles.Count - 1];
+        tiles.ForEach(t => t.Start());
+    }
+
+    Tile AddTile(int position, Tile prevTile)
+    {
+        GameObject tileGO = new GameObject("Tile");
+        tileGO.AddComponent<SpriteRenderer>().sprite = Sprites.PickRandom();
+        tileGO.transform.position = new Vector2(position, transform.position.y);
+        tileGO.transform.SetParent(transform);
+        Tile tile = tileGO.AddComponent<Tile>();
+        tile.Initialise(() => ResetTile(tile), tileWidth, Speed, prevTile);
+        return tile;
+    }
+
+
+    void ResetTile(Tile tile)
+    {
+        tile.GetComponent<SpriteRenderer>().sprite = Sprites.PickRandom();
+        tile.transform.position = new Vector2(tile.PrevTileRightX(), transform.position.y);
     }
 
     void Update()
---
+++ Assets/Resources/Scripts/Enviroment/Tile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private Action onLeaveScreen;
+    private int width;
+    private Vector3 TargetPosition;
+
+    private Vector3 NonRoundedCurrentPosition;
+    private float speed;
+    private bool start = false;
+    public Tile PrevTile;
+
+    public void Initialise(Action onLeaveScreen, int width, float speed, Tile prevTile) {
+        this.onLeaveScreen = onLeaveScreen;
+        this.width = width;
+        this.speed = speed;
+        TargetPosition = new Vector3(-width, transform.position.y, transform.position.z);
+        NonRoundedCurrentPosition = transform.position;
+        this.PrevTile = prevTile;
+    }
+
+    public void Reset()
+    {
+        NonRoundedCurrentPosition = transform.position;
+    }
+
+
+    public void Start()
+    {
+        start = true;
+    }
+
+    public float PrevTileRightX() => PrevTile.transform.position.x + (width);
+
+    void Update()
+    {
+        if (!start)
+        {
+            return;
+        }
+        if (transform.position == TargetPosition)
+        {
+            onLeaveScreen();
+            Reset();
+        }
+
+        float step = speed * Time.deltaTime;
+        NonRoundedCurrentPosition = Vector3.MoveTowards(NonRoundedCurrentPosition, TargetPosition, step);
+
+        transform.position = new Vector3(Mathf.RoundToInt(NonRoundedCurrentPosition.x), transform.position.y, transform.position.z);
+
+        if (PrevTile != null ){
+            float newX = PrevTileRightX();
+
+            float diffBetweenPrev = transform.position.x - newX;
+            if (diffBetweenPrev >= 1 && diffBetweenPrev <= width)
+            {
+                // out of line so reposition
+                transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+            }
+
+        }
+       
+       
+    }
+}
