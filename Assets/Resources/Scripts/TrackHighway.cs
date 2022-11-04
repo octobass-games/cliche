eb7a1dafcde77d54c88dbb0fdad084f455e491ba
adamskipadamski <adam.t.wright@outlook.com>
2022-11-04 15:33:18 +0000

    make TrackHighway set arrow sprite

--- Assets/Resources/Scripts/TrackHighway.cs
+++ Assets/Resources/Scripts/TrackHighway.cs
@@ -8,6 +8,11 @@ public class TrackHighway : MonoBehaviour
     public float DownYCoord;
     public float LeftYCoord;
 
+    public Sprite UpSprite;
+    public Sprite RightSprite;
+    public Sprite DownSprite;
+    public Sprite LeftSprite;
+
     public void PlaceNotes(List<Note> notes)
     {
         for (int i = 0; i < notes.Count; i++)
@@ -16,6 +21,8 @@ public class TrackHighway : MonoBehaviour
 
             note.transform.SetParent(transform);
             note.transform.position = new Vector3(note.InitialX, GetYCoordForNoteType(note.NoteType), 0);
+
+            note.sprite.sprite = GetSpriteForNoteType(note.NoteType);
         }
     }
 
@@ -31,6 +38,23 @@ public class TrackHighway : MonoBehaviour
                 return DownYCoord;
             case NoteType.LEFT:
                 return LeftYCoord;
+            default:
+                throw new System.Exception("Invalid NoteType");
+        }
+    }
+
+    private Sprite GetSpriteForNoteType(NoteType noteType)
+    {
+        switch (noteType)
+        {
+            case NoteType.UP:
+                return UpSprite;
+            case NoteType.RIGHT:
+                return RightSprite;
+            case NoteType.DOWN:
+                return DownSprite;
+            case NoteType.LEFT:
+                return LeftSprite;
             default:
                 throw new System.Exception("Invalid NoteType");
         }
