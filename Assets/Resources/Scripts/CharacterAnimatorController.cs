b5ab414426a788e517e455e5a1954a458af6d4ed
Megan <megan@theparkes.me.uk>
2022-11-04 11:02:53 +0000

    update character animator

---
+++ Assets/Resources/Scripts/CharacterAnimatorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterAnimatorController : MonoBehaviour
+{
+    public Animator Animator;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ResetAnimator()
+    {
+        Animator.SetInteger("Dance", 0);
+    }
+
+
+    public void RandomDance()
+    {
+        int danceN = Random.Range(1, 5);
+        Animator.SetInteger("Dance", danceN);
+    }
+}
