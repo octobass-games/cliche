43f62da5c2cf45e5ccfc789ec9bfb5ac560ab95d
Megan <megan@theparkes.me.uk>
2022-11-08 19:27:01 +0000

    siren intor animation

---
+++ Assets/Resources/Scripts/Enviroment/Fall.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    public float CustomSpeed = 0;
+
+    private Vector3 ScenePosition;
+    private Vector3 TargetPosition;
+    private bool IsFalling = false;
+
+    void Awake()
+    {
+        ScenePosition = transform.position;
+        CustomSpeed = CustomSpeed == 0 ? Random.Range(50f, 150f) : CustomSpeed;
+        FallIn();
+    }
+
+    void Update()
+    {
+        if (IsFalling)
+        {
+            fall();
+        }
+    }
+
+    private void fall()
+    {
+        float step = CustomSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, step);
+
+        if (transform.position == TargetPosition)
+        {
+            IsFalling = false;
+        }
+    }
+
+
+    public void FallIn()
+    {
+        TargetPosition = ScenePosition;
+        transform.position = ScenePosition + new Vector3(0, 100, 0);
+        IsFalling = true;
+    }
+
+    public void FallOut()
+    {
+        TargetPosition = transform.position - new Vector3(0, 400, 0);
+        IsFalling = true;
+    }
+}
