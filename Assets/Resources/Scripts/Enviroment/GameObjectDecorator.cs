be7f396a51b93bd684a5790a78b5907c56a57d4c
Megan <megan@theparkes.me.uk>
2022-11-19 15:13:29 +0000

    update home scene

---
+++ Assets/Resources/Scripts/Enviroment/GameObjectDecorator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameObjectDecorator : MonoBehaviour
+{
+    public GameObject Obj;
+    public void Toggle()
+    {
+        Obj.SetActive(!Obj.activeSelf);
+    }
+}
