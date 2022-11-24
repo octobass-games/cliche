af2f592b2dea3fc25fec08fb0bab52b618e644cb
Megan <megan@theparkes.me.uk>
2022-11-24 14:06:21 +0000

    update forest

--- Assets/Resources/Scripts/Enviroment/GameObjectDecorator.cs
+++ Assets/Resources/Scripts/Enviroment/GameObjectDecorator.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameObjectDecorator : MonoBehaviour
 {
     public GameObject Obj;
+    public UnityEvent OnAwakeEvent;
     public void Toggle()
     {
         Obj.SetActive(!Obj.activeSelf);
+    }
+
+    void Awake()
+    {
+        if (OnAwakeEvent != null)
+        {
+            OnAwakeEvent.Invoke();
+        }
     }
 }
