9ff289393198a22ad92fb3ea2f12e6679808e1d2
Megan <megan@theparkes.me.uk>
2022-11-28 17:40:25 +0000

    add extra events

--- Assets/Resources/Scripts/EventInvoker.cs
+++ Assets/Resources/Scripts/EventInvoker.cs
@@ -6,8 +6,12 @@ public class EventInvoker : MonoBehaviour
     public UnityEvent Event;
     public UnityEvent Event2;
     public UnityEvent Event3;
+    public UnityEvent Event4;
+    public UnityEvent Event5;
 
     public void InvokeCustomEvent() => Event.Invoke();
     public void InvokeCustomEvent2() => Event2.Invoke();
     public void InvokeCustomEvent3() => Event3.Invoke();
+    public void InvokeCustomEvent4() => Event4.Invoke();
+    public void InvokeCustomEvent5() => Event5.Invoke();
 }
