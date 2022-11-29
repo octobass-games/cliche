ea8d1a7734a9e4ebf20a2fcdeb6f1cfe55f936e9
Megan <megan@theparkes.me.uk>
2022-11-29 14:03:06 +0000

    add dialogue to forest scene

---
+++ Assets/Resources/Scripts/Enviroment/AnimatorDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AnimatorDecorator : MonoBehaviour
+{
+    private Animator Animator;
+    // Use this for initialization
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+
+    public void SetBoolToTrue(string boolName)
+    {
+        Animator.SetBool(boolName, true);
+    }
+
+    public void SetBoolToFalse(string boolName)
+    {
+        Animator.SetBool(boolName, false);
+
+    }
+}
