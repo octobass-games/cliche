28c85ef54a52dfb8e3dcd137d43aa91245635546
adamskipadamski <adam.t.wright@outlook.com>
2022-11-30 12:03:10 +0000

    add Loading scene

---
+++ Assets/Resources/Scripts/BankLoader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BankLoader : MonoBehaviour
+{
+    [FMODUnity.BankRef]
+    public List<string> banks;
+
+    public void Start()
+    {
+        foreach (string b in banks)
+        {
+            FMODUnity.RuntimeManager.LoadBank(b, true);
+        }
+        /*
+            For Chrome / Safari browsers / WebGL.  Reset audio on response to user interaction (LoadBanks is called from a button press), to allow audio to be heard.
+        */
+        FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+        FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+
+        StartCoroutine(CheckBanksLoaded());
+    }
+
+    IEnumerator CheckBanksLoaded()
+    {
+        while (!FMODUnity.RuntimeManager.HaveAllBanksLoaded)
+        {
+            yield return null;
+        }
+
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
